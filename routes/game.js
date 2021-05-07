1f7be13ba407dddf9b23936750cfb9ceda5203d7
Ryan Fritz <ryanfritzc@gmail.com>
2021-05-07 18:15:03 -0400

    finished level 1 and 2

--- routes/game.js
+++ routes/game.js
@@ -4,4 +4,17 @@ const UserController = require('../controllers/UserController.js');
 
 router.get('/', UserController.play);
 
+router.get('/lvl1', function (req, res) {
+    res.render('lvl1');
+  });
+router.get('/lvl2', function (req, res) {
+    res.render('lvl2');
+  });
+router.get('/lvl3', function (req, res) {
+    res.render('lvl3');
+  });
+router.get('/lvl4', function (req, res) {
+    res.render('lvl4');
+  });
+
 module.exports = router;
